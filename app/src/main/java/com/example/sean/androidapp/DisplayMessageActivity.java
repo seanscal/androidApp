13aa0d58a32629dca849ec0508a6003d59a92b53
Sean Scalabrini <sscalabrini@digitallumens.com>
2015-03-18 10:06:26 -0400

    d

--- app/src/main/java/com/example/sean/androidapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/sean/androidapp/DisplayMessageActivity.java
@@ -13,7 +13,6 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Get the message from the intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
